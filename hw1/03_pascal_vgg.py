853362d2178bf175c837b28f45a06057b123cb5d
mansi <mmane@andrew.cmu.edu>
2018-03-05 21:27:10 -0500

    adding vgg

--- hw1/03_pascal_vgg.py
+++ hw1/03_pascal_vgg.py
@@ -404,8 +404,7 @@ def summary_var_lr(log_dir, name, val, step):
     writer.add_summary(summary_proto, step)
     writer.flush()
 
-def main():
-    args = parse_args()
+def main():    args = parse_args()
 
     # Load training and eval data
     # train_data, train_labels, train_weights = load_pascal(
@@ -489,6 +488,7 @@ def main():
 
         summary_var(log_dir=log_dir,
                     name="mAP", val=np.mean(AP), step=i*no_of_steps)
+
 
 
     #######  Image logging
