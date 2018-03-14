92259af051bad1688967423e427ae8f524b8e23b
mansi <mmane@andrew.cmu.edu>
2018-03-13 23:26:39 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # visual_learning_and_recognition
+Homework 1: Object Classification with TensorFlow!
+
+	1.MNIST 10-digit classification in TensorFlow with Estimator APIs
+	2.2-layer network for PASCAL multi-label classification
+	3.AlexNet for PASCAL classification
+	4.VGG-16 for PASCAL classification
+	5.finetuning from ImageNet
+
+Homework 2: Weakly Supervised Object Localization
+
+	We will implement two approaches in this assignment:
+	1.Oquab, Maxime, et al. "Is object localization for free?-weakly-supervised learning with convolutional neural networks." Proceedings of the IEEE Conference on Computer Vision and Pattern Recognition. 2015.
+	2.Bilen, Hakan, and Andrea Vedaldi. "Weakly supervised deep detection networks." Proceedings of the IEEE Conference on Computer Vision and Pattern Recognition. 2016.
--- hw1/05_01_weights.py
+++ hw1/05_01_weights.py
@@ -16,36 +16,27 @@ from eval import compute_map
 #import models
 from tensorflow.core.framework import summary_pb2
 from funcs import *
+import tensorflow as tf
 tf.logging.set_verbosity(tf.logging.INFO)
 
-def main():
-    readerf = tf.train.NewCheckpointReader("/tmp/02_pascal_model_scratch_bak/model.ckpt-40000")
-    readerm = tf.train.NewCheckpointReader("./plots/02_pascal_alexnet_iters60/model.ckpt-24000")
-    readers = tf.train.NewCheckpointReader("./plots/02_pascal_alexnet_iters100/model.ckpt-40000")
+readerf = tf.train.NewCheckpointReader("/tmp/02_pascal_model_scratch_bak/model.ckpt-40000")
 
-    x30 = readerf.get_tensor("conv2d/kernel")
-    x60 = readerm.get_tensor("conv2d/kernel")
-    x100 = readers.get_tensor("conv2d/kernel")
-    filters30=put_kernels_on_grid (tf.convert_to_tensor(x30), pad = 1)
-    filters60=put_kernels_on_grid (tf.convert_to_tensor(x60), pad = 1)
-    filters100=put_kernels_on_grid (tf.convert_to_tensor(x100), pad = 1)
-
-    im_summary30=tf.summary.image('filters30', filters30)
-    im_summary60=tf.summary.image('filters60', filters60)
-    im_summary100=tf.summary.image('filters100', filters100)
-
-    all_summary = tf.summary.merge([im_summary30, im_summary60, im_summary100])
+f = readerf.get_tensor("conv2d/kernel")
 
 
+final_filt=put_kernels_on_grid (tf.convert_to_tensor(f), pad = 1)
 
 
-    with tf.Session() as sess:
-        # Run
-        summary = sess.run(all_summary)
-        # Write summary
-        writer = tf.summary.FileWriter(DIR)
-        writer.add_summary(summary)
-        writer.close()
+im_summary=tf.summary.image('final_filt', final_filt)
 
-if __name__ == "__main__":
-    main()
+
+all_summary = tf.summary.merge([im_summary])
+
+
+with tf.Session() as sess:
+    summary = sess.run(all_summary)
+    writer = tf.summary.FileWriter('./fit')
+    writer.add_summary(summary)
+    writer.close()
+
+
